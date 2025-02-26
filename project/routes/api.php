6e124c828a8c56c74f654eb65f8399c8c298bf9d
Gabriel Fiami <gabrielfiami.dev@gmail.com>
2025-02-26 17:27:55 -0300

    rota api de download

--- project/routes/api.php
+++ project/routes/api.php
@@ -10,4 +10,6 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/upload', [FileController::class, 'upload']);
 Route::get('/history', [FileController::class, 'history']);
+Route::get('/download/{filehash}', [FileController::class, 'download'])->name('download');
+
 
