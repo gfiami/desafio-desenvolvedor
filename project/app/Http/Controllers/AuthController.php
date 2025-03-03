328ba8650671f01bc058486f7c85e74472759506
Gabriel Fiami <gabrielfiami.dev@gmail.com>
2025-03-03 17:56:56 -0300

    autenticação para consumir endpoints

---
+++ project/app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function signup(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'Usuário registrado com sucesso',
+            'user' => $user
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if (auth()->attempt($credentials)) {
+            $user = auth()->user();
+            $token = $user->createToken('Personal Access Token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login bem-sucedido',
+                'token' => $token,
+            ], 200);
+        }
+
+        return response()->json(['error' => 'Credenciais inválidas'], 401);
+    }
+}
+
--- project/routes/api.php
+++ project/routes/api.php
@@ -3,14 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FileController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/upload', [FileController::class, 'upload'])->middleware('auth:sanctum');
+Route::get('/history', [FileController::class, 'history'])->middleware('auth:sanctum');
+Route::get('/content/{filehash}', [FileController::class, 'content'])->name('content')->middleware('auth:sanctum');
 
-Route::post('/upload', [FileController::class, 'upload']);
-Route::get('/history', [FileController::class, 'history']);
-Route::get('/content/{filehash}', [FileController::class, 'content'])->name('content');
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
 
 
 
